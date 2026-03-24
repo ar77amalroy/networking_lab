b949ab737c92674822031c10e1b1850acff56d95
Amal Roy <161358600+ar77amalroy@users.noreply.github.com>
2026-03-25 04:58:30 +0530

    Update leakybucket.c

--- leakybucket.c
+++ leakybucket.c
@@ -1,27 +1,25 @@
 #include<stdio.h>
-int main(){
-    int incoming, outgoing, buck_size, n, store = 0;
-    printf("Enter bucket size, outgoing rate and no of inputs : ");
-    scanf("%d %d %d", &buck_size, &outgoing, &n);
-    while (n != 0) {
-        printf("Enter the incoming packet size : ");
+int main() {
+    int incoming, outgoing, bucket_size, n, store = 0;
+    printf("Enter bucket size, outgoing rate and no of inputs: ");
+    scanf("%d %d %d", &bucket_size, &outgoing, &n);
+    while(n > 0) {
+        printf("Enter incoming packet size: ");
         scanf("%d", &incoming);
-        printf("Incoming packet size %d\n", incoming);
-        if (incoming <= (buck_size - store)){
+        printf("Incoming packet size: %d\n", incoming);
+        if(incoming <= (bucket_size - store)) {
             store += incoming;
-            printf("Bucket buffer size :  %d out of %d\n", store, buck_size);
         } else {
-            printf("Dropped %d no of packets\n", incoming - (buck_size - store));
-            store = buck_size;
-            printf("Bucket buffer size :  %d out of %d\n", store, buck_size);
+            printf("Dropped packets: %d\n", incoming - (bucket_size - store));
+            store = bucket_size;
         }
-        if(store>=outgoing){
-        store=outgoing-store;
-        }
-        if(store<0){
-        store=store*(-1);
-        }
-        printf("After outgoing ,  There are %d  out of %d packets left in buffer\n", store, buck_size);
+        printf("Bucket: %d/%d\n", store, bucket_size);
+        if(store >= outgoing)
+            store -= outgoing;
+        else
+            store = 0;
+        printf("After outgoing: %d packets left\n\n", store);
         n--;
     }
+    return 0;
 }
