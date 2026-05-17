c7831df03d93fcd3cbdae6342798849e768f69e4
Amal Roy <161358600+ar77amalroy@users.noreply.github.com>
2026-05-17 20:36:41 +0530

    Update dvr.c

--- dvr.c
+++ dvr.c
@@ -35,3 +35,41 @@ int main() {
     }
     return 0;
 }
+#############################################
+#include <stdio.h>
+int main()
+{
+    int c[10][10], d[10][10], next[10][10];
+    int n, i, j, k, ch;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    printf("Enter cost matrix:\n");
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &c[i][j]);
+            d[i][j] = c[i][j];
+            next[i][j] = j;
+        }
+    do
+    {
+        ch = 0;
+        for(i = 0; i < n; i++)
+            for(j = 0; j < n; j++)
+                for(k = 0; k < n; k++)
+                    if(d[i][j] > c[i][k] + d[k][j])
+                    {
+                        d[i][j] = c[i][k] + d[k][j];
+                        next[i][j] = k;
+                        ch = 1;
+                    }
+    } while(ch);
+    for(i = 0; i < n; i++)
+    {
+        printf("\nRouter %d:\n", i + 1);
+        for(j = 0; j < n; j++)
+            printf("To %d via %d dist %d\n",
+                   j + 1, next[i][j] + 1, d[i][j]);
+    }
+    return 0;
+}
